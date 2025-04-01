5bfa4bf1ae571e04deda1bd9ffb79fb35b8c624c
Danny Nygård Hansen <dannynhansen@gmail.com>
2025-04-01 10:59:07 +0200

    More Python.

--- create_blob.py
+++ create_blob.py
@@ -1,11 +1,17 @@
 import hashlib
 import zlib
 
-content = "what is up, doc?"
-header = f"blob {len(content.encode('utf-8'))}\0"
+content = "what is up, doc?".encode('utf-8')
+header = f"blob {len(content)}\0".encode('utf-8')
 store = header + content
 
 m = hashlib.sha1()
-m.update(store.encode('utf-8'))
-print(m.hexdigest())
-print(zlib.compress(store.encode('utf-8')))
+m.update(store)
+sha1 = m.hexdigest()
+
+path = f".git/objects/{sha1[0:2]}/{sha1[2:]}"
+
+print(sha1)
+print(path)
+zlib_content = zlib.compress(store)
+print(zlib.decompress(zlib_content))
